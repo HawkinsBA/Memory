4056b75a0b633cd07c1f95d7909d5cf25941257f
Blake <blake.hawkins97@gmail.com>
2019-04-18 10:10:35 -0500

    Initial commit

---
+++ Assets/Scripts/EncounterController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EncounterController : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float startx, starty, startz;
+        startx = float.Parse(PlayerPrefs.GetString("StartX"));
+        starty = float.Parse(PlayerPrefs.GetString("StartY"));
+        startz = float.Parse(PlayerPrefs.GetString("StartZ"));
+        Vector3 playerPosition = new Vector3(startx, starty, startz);
+        player.transform.position = playerPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            PlayerPrefs.SetString("StartX", player.transform.position.x.ToString());
+            PlayerPrefs.SetString("StartY", player.transform.position.y.ToString());
+            PlayerPrefs.SetString("StartZ", player.transform.position.z.ToString());
+            SceneManager.LoadScene("Battle Placeholder");
+        }
+    }
+
+    void setupEncounter()
+    {
+        //Move the player and instantiate the enemy.
+        //We can save the player's previous position through PlayerPrefs 
+        //and have them placed where they were before the fight after the battle concludes.
+    }
+}
